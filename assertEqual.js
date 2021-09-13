cd9b35ce0c109345d01ccaefb7a5fe6e388b754e
Tom Zhang <tom.zhang989@gmail.com>
2021-09-13 16:15:41 -0400

    created a colorful assert equal

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,24 @@
+// Assert equal takes 2 values(actual, expected)
+// and compares them to each other. If they
+// are equivalent, it logs a success message to console
+// or a failiure message if a fail
+
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
 
 };
 
 // TEST CODE
+/*
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual('test', 'test');
+assertEqual('1', 1);
+assertEqual(true, 1);
+*/
