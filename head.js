9f2ee552457f6728362eef5595965c307130fe96
Tom Zhang <tom.zhang989@gmail.com>
2021-09-13 16:27:29 -0400

    finished writing the head.js function

--- head.js
+++ head.js
@@ -10,3 +10,17 @@ const assertEqual = function(actual, expected) {
 // function head.js returns the first
 // element in an array or undefined
 // if the array is empty
+// arrayOf var -> var
+const head = (anArr) => {
+  return anArr[0];
+};
+
+
+/*
+assertEqual(head([1,3]), head([1]));
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+assertEqual(head([]), undefined);
+*/
