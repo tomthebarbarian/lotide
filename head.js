63e558e67b51ed5b91d2fef9c48e3888fe5d5d84
Tom Zhang <tom.zhang989@gmail.com>
2021-09-13 16:22:07 -0400

    created head.js and pasted assertEqual into it

---
+++ head.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// function head.js returns the first
+// element in an array or undefined
+// if the array is empty
