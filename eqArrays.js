1f90643715dd1636d7225092db70bc1a8ddf2a8e
Tom Zhang <tom.zhang989@gmail.com>
2021-09-14 19:15:54 -0400

    created eqArrays.js and added first few lines

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = (inArr1, inArr2) => {
+  let ans = false;
+  if (inArr1.length === inArr2.length) {
+    for (let i = 0; i < inArr1.length; i++) {
+      if (inArr1[i] !== inArr2) {
+        break;
+      }
+    }
+  }
+
+  return ans;
+};
+ 
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
