e4b017c0efb0d2ab966248941bcb079a8baa443b
Tom Zhang <tom.zhang989@gmail.com>
2021-09-14 19:21:43 -0400

    finished testing all given arrays

--- eqArrays.js
+++ eqArrays.js
@@ -10,22 +10,31 @@ const assertEqual = function(actual, expected) {
 const eqArrays = (inArr1, inArr2) => {
   let ans = false;
   if (inArr1.length === inArr2.length) {
-    for (let i = 0; i < inArr1.length; i++) {
-      if (inArr1[i] !== inArr2) {
+    for (let i = 0; i <= inArr1.length; i++) {
+      if (i === inArr1.length) {
+        ans = true;
+        break;
+      } else if (inArr1[i] !== inArr2[i]) {
         break;
       }
     }
   }
-
   return ans;
 };
  
+//Testing
+/*
+const test1 = eqArrays([1, 2, 3], [1, 2, 3]); // => true
+const test2 = eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+const test3 = eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+const test4 = eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+assertEqual(test1, true);
+assertEqual(test2, false);
+assertEqual(test3, true);
+assertEqual(test4, false);
+*/
