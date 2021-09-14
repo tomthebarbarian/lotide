054c412df41f3620d82f990f4bc1170177435af7
Tom Zhang <tom.zhang989@gmail.com>
2021-09-14 19:33:21 -0400

    created without js and some comments

---
+++ without.js
@@ -0,0 +1,4 @@
+// function without takes an input array (inArr) and an array of values (removes)and
+// returns a new array equivalent to the inputarray with all values of exlude
+// array excluded
+
