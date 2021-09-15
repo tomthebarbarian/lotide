4b9d9af9766bf58b6a82dc4d786d41b6e1f6c7a3
Tom Zhang <tom.zhang989@gmail.com>
2021-09-14 20:01:28 -0400

    created/inited flatten.js challenge

---
+++ flatten.js
@@ -0,0 +1,24 @@
+const eqArrays = (inArr1, inArr2) => {
+  let ans = false;
+  if (inArr1.length === inArr2.length) {
+    for (let i = 0; i <= inArr1.length; i++) {
+      if (i === inArr1.length) {
+        ans = true;
+        break;
+      } else if (inArr1[i] !== inArr2[i]) {
+        break;
+      }
+    }
+  }
+  return ans;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1,arr2) === true) {
+    console.log(`🟢🟢🟢 Equal Arrays Assertion Passed`);
+  } else {
+    console.log(`🔴🔴🔴 Different Arrays Assertion Failed`);
+  }
+};
+
+// function flatten
