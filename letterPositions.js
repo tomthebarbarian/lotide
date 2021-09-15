595bf3de75ed44a6cf05f29ca19c93a4a7cce63f
Tom Zhang <tom.zhang989@gmail.com>
2021-09-15 15:54:44 -0400

    created basic functions for letterPositions()

---
+++ letterPositions.js
@@ -0,0 +1,45 @@
+const eqArrays = (inArr1, inArr2) => {
+  let ans = false;
+  if (inArr1.length === inArr2.length) {
+    for (let i = 0; i <= inArr1.length; i++) {
+      if (i === inArr1.length) {
+        ans = true;
+        break;
+      } else if (inArr1[i] !== inArr2[i]) {
+        break;
+      }
+    }
+  }
+  return ans;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1,arr2) === true) {
+    console.log(`🟢🟢🟢 Equal Arrays Assertion Passed`);
+  } else {
+    console.log(`🔴🔴🔴 Different Arrays Assertion Failed`);
+  }
+};
+
+//  Function letterPositions takes in a string(sentence) and returns an object
+// containing the location index(es) of each letter
+// str -> obj
+const letterPositions = (sentence) => {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]] === undefined) {
+      results[sentence[i]] = [i];
+    } else {
+      results[sentence[i]].push(i);
+    }
+  }
+  // logic to update results here
+  return results;
+};
+
+
+// Tests
+const testObj = letterPositions('hello');
+assertArraysEqual(testObj[h], [0]);
+assertArraysEqual(testObj[e], [1]);
+assertArraysEqual(testObj[l], [2,3]);
