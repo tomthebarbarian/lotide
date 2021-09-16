3eaff42f101c435f4a7c3f976bea41a4588f3621
Tom Zhang <tom.zhang989@gmail.com>
2021-09-16 14:57:54 -0400

    created findkey and a few lines of starting instructions

---
+++ findKey.js
@@ -0,0 +1,7 @@
+// Implement the function findKey which takes in an object (inObj)and a function (callback)
+//  and returns the first key for which the callback returns a truthy value.
+// If no key is found, then findKey returns undefined.
+
+const findKey = (inObj, callback) = {
+  
+}
