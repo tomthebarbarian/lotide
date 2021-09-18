7cf4a9d4bee00b0a2fd2a5db89a72910c94883cb
Tom Zhang <tom.zhang989@gmail.com>
2021-09-17 21:31:11 -0400

    refactored function to be simpler

--- eqArrays.js
+++ eqArrays.js
@@ -11,17 +11,13 @@ const eqArrays = (inArr1, inArr2) => {
 
   console.log('Array1: ', inArr1, " Array 2: ", inArr2);
   // Not arr
-  if (!(Array.isArray(inArr1))) {
-    return (inArr1 === inArr2);
-  } else if (inArr1.length !== inArr2.length) {
+  if (inArr1.length !== inArr2.length) {
     return false;
   } else {
     for (let i = 0; i < inArr1.length; i++) {
-      console.log('Array1: ', inArr1[i], " Array 2: ", inArr2[i]);
+      // console.log('Array1: ', inArr1[i], " Array 2: ", inArr2[i]);
       if (Array.isArray(inArr1[i])) {
-        if (eqArrays(inArr1[i], inArr2[i])) {
-          continue;
-        } else {
+        if (!eqArrays(inArr1[i], inArr2[i])) {
           return false;
         }
       } else if (inArr1[i] !== inArr2[i]) {
