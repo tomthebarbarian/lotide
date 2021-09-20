e30277de699e77b660f81c56d4006932111d5947
Tom Zhang <tom.zhang989@gmail.com>
2021-09-20 14:18:37 -0400

    exporting assertArraysEqual and requireing eqArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,5 @@
-const eqArrays = (inArr1, inArr2) => {
-  let ans = false;
-  if (inArr1.length === inArr2.length) {
-    for (let i = 0; i <= inArr1.length; i++) {
-      if (i === inArr1.length) {
-        ans = true;
-        break;
-      } else if (inArr1[i] !== inArr2[i]) {
-        break;
-      }
-    }
-  }
-  return ans;
-};
+const eqArrays = require("./eqArrays");
+
 
 // function assertArraysEqual takes in 2 arrays and logs a success message to
 // the console if the arrays are equal or an error message otherwise.
@@ -22,3 +10,5 @@ const assertArraysEqual = (arr1, arr2) => {
     console.log(`🔴🔴🔴 Different Arrays Assertion Failed`);
   }
 };
+
+module.exports = (assertArraysEqual);
