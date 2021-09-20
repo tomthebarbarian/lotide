ff2a1df8a194e91a48d9939e5737ac518cb30783
Tom Zhang <tom.zhang989@gmail.com>
2021-09-20 17:45:10 -0400

    made flatten.js recursive by luck

--- flatten.js
+++ flatten.js
@@ -13,31 +13,48 @@ const eqArrays = (inArr1, inArr2) => {
   return ans;
 };
 
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1,arr2) === true) {
-    console.log(`🟢🟢🟢 Equal Arrays Assertion Passed`);
-  } else {
-    console.log(`🔴🔴🔴 Different Arrays Assertion Failed`);
-  }
-};
-
+const assertArraysEqual = require('./assertArraysEqual');
 // function flatten takes an array of arrays/including nested arrays of n levels and
 // and returns a flattened version of the array.
 
-const flatten = (inArr) => {
-  let ansArr = [];
-  for (let elem of inArr) {
-    if (Array.isArray(elem)) {
-      for (let x of elem) {
-        ansArr.push(x);
-      }
-    } else {
-      ansArr.push(elem);
+const flatten = (inArr, outArr = []) => {
+  // single val
+  // console.log(outArr);
+  if (!Array.isArray(inArr)) {
+    return outArr.push(inArr);
+    // An arr
+  } else {
+
+    // Single elem
+    if (inArr.length === 1) {
+      outArr.concat(flatten(inArr[0],outArr));
+      return outArr;
     }
+    // rest
+    if (inArr.length > 1) {
+      outArr.concat(flatten(inArr[0],outArr));
+      return flatten(inArr.slice(1), outArr);
+    }
+    
+
+    // return flatten(inArr[0], outArr).join(flatten(inArr.slice(1),outArr), '');
+
+
+  // for (let elem of inArr) {
+  //   if (Array.isArray(elem)) {
+  //     for (let x of elem) {
+  //       ansArr.push(x);
+  //     }
+  //   } else {
+  //     ansArr.push(elem);
+  //   }
+  // }
+  // return outArr;
   }
-  return ansArr;
 };
 
 
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
-// come back when you know recursion.
+console.log(flatten([1, 2, [[3], 4], 5, [6]]));
+console.log(flatten(["😎", [["💩", ["🤗"]], [[["😼"]], "😂"]]]));
+
+// come back when you know recursion
