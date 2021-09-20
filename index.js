8873e3fc10dc100de5136f4c6712ecef6f612361
Tom Zhang <tom.zhang989@gmail.com>
2021-09-20 18:22:38 -0400

    added some more things to index

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const flatten = require('./flatten');
+const countOnly = require('./countOnly');
+const letterPositions = require('./letterPositions');
 
 module.exports = {
   head,
   tail,
-  middle
+  middle,
+  flatten,
+  countOnly,
+  letterPositions
 };
